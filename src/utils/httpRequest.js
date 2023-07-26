d0d171d05ecb089a974575d9aeccf2dc448a1250
Đào Đức Mạnh <manhdao1006@gmail.com>
2023-07-26 09:25:56 +0700

    Giải quyết vấn đề nhiều file index.js

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_BASE_URL);
+// console.log(process.env.REACT_APP_BASE_URL);
 
 const httpRequest = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
